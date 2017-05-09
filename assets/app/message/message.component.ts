9b4b275592eca2a0582f8667072edf76f1eb5765
Shangwu Yao <shangwuyao@gmail.com>
2017-05-09 10:02:23 +0800

    Added Event binding

--- assets/app/message/message.component.ts
+++ assets/app/message/message.component.ts
@@ -1,9 +1,30 @@
-import {Component, Input} from "@angular/core";
+import {Component, EventEmitter, Input, Output} from "@angular/core";
 import {Message} from "./message.model"
 @Component({
     selector: 'app-message',
-    templateUrl: './message.component.html'
+    templateUrl: './message.component.html',
+    styles: [`
+        .author {
+            display: inline-block;
+            font-style: italic;
+            font-size: 12px;
+            width: 80%;
+        }
+        .config {
+            display: inline-block;
+            text-align: right;
+            font-size: 12px;
+            width: 19%;
+        }
+    `]
 })
 export class MessageComponent {
     @Input() message: Message;
+    @Output() editClicked = new EventEmitter<string>();
+
+    color = 'green';
+
+    onEdit() {
+        this.editClicked.emit('A new value');
+    }
 }
