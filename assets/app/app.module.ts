a6c8d1f62da3775888840f51f475e709935c8a82
Shangwu Yao <shangwuyao@gmail.com>
2017-05-09 12:03:40 +0800

    added header

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -6,13 +6,19 @@ import {FormsModule} from "@angular/forms";
 import {MessageComponent} from "./message/message.component";
 import {MessageListComponent} from "./message/message-list.component";
 import {MessageInputComponent} from "./message/message-input.component";
+import {MessagesComponent} from "./message/messages.component";
+import {AuthenticationComponent} from "./auth/authentication.component";
+import {HeaderComponent} from "./header.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
