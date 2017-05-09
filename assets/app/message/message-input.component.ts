ef931d1434421cf708440dfb49f44d3a009aa1e2
Shangwu Yao <shangwuyao@gmail.com>
2017-05-09 11:48:34 +0800

    use form for submitting

--- assets/app/message/message-input.component.ts
+++ assets/app/message/message-input.component.ts
@@ -1,6 +1,7 @@
 import {Component} from "@angular/core";
 import {MessageService} from "./message.service";
 import {Message} from "./message.model";
+import {NgForm} from "@angular/forms";
 
 @Component({
     selector: 'app-message-input',
@@ -14,8 +15,10 @@ export class MessageInputComponent {
     constructor(private messageService: MessageService) {
 
     }
-    onSave(value: string){
-        const message = new Message(value, 'Tom');
+    onSubmit(form: NgForm){
+        console.log(form);
+        const message = new Message(form.value.content, 'Tom');
         this.messageService.addMessage(message);
+        form.resetForm();
     }
 }
